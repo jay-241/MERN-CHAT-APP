82e818f036ee8d4b69c63da46004f08710e73070
jnrana <jy834177@dal.ca>
2024-09-05 14:01:01 -0300

    Remove frontend submodule and add as regular folder

---
+++ frontend/src/Pages/ChatPage.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+const ChatPage = () => {
+  const [chats, setChats] = useState([]);
+
+  const fetchChats = async () => {
+    const { data } = await axios.get("/api/chat");
+    setChats(data);
+    // console.log(data)
+  };
+
+  useEffect(() => {
+    fetchChats();
+  }, []);
+
+  return (
+    <div>
+      {chats.map((chat) => {
+        return (
+          <div className="chat" key={chat._id}>
+            {chat.chatName}{" "}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ChatPage;
